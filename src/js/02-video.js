fdf8ed1cc1b6a5ba1011a99d8034eb6a3d021086
Oleksandr <110254286+OleksandrUA777@users.noreply.github.com>
2022-12-25 13:25:56 +0100

    add files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from "@vimeo/player"
+import throttle from "lodash.throttle";
 
+const SECONDS_KEY = "videoplayer-current-time"
+
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);  
+
+player.on('timeupdate', throttle(timeChangeHandler,1000)) 
+function timeChangeHandler(currentTime){
+    const seconds = currentTime.seconds
+    localStorage.setItem(SECONDS_KEY,seconds)
+}
+const savedData = localStorage.getItem(SECONDS_KEY)
+
+if(savedData){
+player.setCurrentTime(savedData).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+})
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from "lodash.throttle"
 
+const DATA_KEY = "feedback-form-state";
+const refs = {
+ form: document.querySelector('.feedback-form'),
+ input: document.querySelector('input[name="email"]'),
+ textarea : document.querySelector('textarea[name = "message"]')
+}
+populateData()
+refs.form.addEventListener('input',throttle(formChangeHandler,500))
+refs.form.addEventListener('submit',formSubmitHandler)
+
+const data = {}
+
+function formChangeHandler(event){
+//Створюю дату та записую в локал сторедж
+    data[event.target.name] = event.target.value
+
+    const dataToString = JSON.stringify(data)
+
+    localStorage.setItem(DATA_KEY, dataToString)
+    
+}
+function populateData(){
+    const savedData = localStorage.getItem(DATA_KEY)
+    const parsedData = JSON.parse(savedData)
+
+//Якщо є данні у сховищі записати їх у форму
+
+    if(savedData){
+        refs.input.value = parsedData.email || ""
+        refs.textarea.value = parsedData.message || ""
+    }
+}
+function formSubmitHandler(event){
+    event.preventDefault();
+    
+//Під час сабміту очистити форму, локал сторедж та вивести у консоль object 
+
+    console.log(JSON.parse(localStorage.getItem(DATA_KEY)))
+
+    event.target.reset();
+
+    localStorage.removeItem(DATA_KEY)
+}
